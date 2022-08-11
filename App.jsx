8eddcaec3d159b67d5ef97c05d4e59ed741f097c
hidems <miura.eleven@gmail.com>
2022-08-11 23:54:39 +0200

    Comment for react/jsx-uses-react

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react'; // No need it current version of React Native
 
 export default function App() {
   return (
