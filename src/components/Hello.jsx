95ced3c150372b1af299a63d9617241511078695
hidems <miura.eleven@gmail.com>
2022-08-14 13:32:08 +0200

    Install and use prop-types

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,13 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { string } from 'prop-types';
 
 function Hello(props) {
-  const children = props.children;
+  const { children } = props;
   return (
     <View>
       <Text style={styles.text}>Hello {children}</Text>
     </View>
   );
 }
+
+Hello.propTypes = {
+  children: string,
+};
 
 const styles = StyleSheet.create({
   text: {
