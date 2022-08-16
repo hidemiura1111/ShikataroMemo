74e070dcf7233845f5a095a5cccdca20cdb76413
hidems <miura.eleven@gmail.com>
2022-08-16 13:56:57 +0200

    Props with style

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
-import { string, bool } from 'prop-types';
+import { string, bool, shape } from 'prop-types';
 
 function Hello(props) {
-  const { children, bang } = props;
+  const { children, bang, style } = props;
   return (
     <View>
-      <Text style={styles.text}>
+      <Text style={[styles.text, style]}>
         {`Hello ${children}${bang ? '!' : ''}`}
       </Text>
     </View>
@@ -15,10 +15,12 @@ function Hello(props) {
 Hello.propTypes = {
   children: string.isRequired,
   bang: bool,
+  style: shape(),
 };
 
 Hello.defaultProps = {
   bang: false,
+  style: null,
 };
 
 const styles = StyleSheet.create({
