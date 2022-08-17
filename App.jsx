785d72282834345ef61982ad5ece0924f22ee01a
hidems <miura.eleven@gmail.com>
2022-08-17 21:50:36 +0200

    Index markup

--- App.jsx
+++ App.jsx
@@ -2,17 +2,58 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 // import React from 'react'; // No need it current version of React Native
 
-import Hello from './src/components/Hello';
+// import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello bang>World</Hello>
-      <Hello style={{ fontSize: 16 }}>Small World</Hello>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Test app!</Text>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
+      <View>
+        <View>
+          <Text>AnnoyingMemo App</Text>
+          <Text>Log out</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>Shopping List</Text>
+            <Text>10:13 17 Aug 22</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>Shopping List</Text>
+            <Text>10:13 17 Aug 22</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>Shopping List</Text>
+            <Text>10:13 17 Aug 22</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
+
     </View>
   );
 }
