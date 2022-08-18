dcb827f0e83060eeabf0455292971a384ef9f87e
hidems <miura.eleven@gmail.com>
2022-08-19 00:02:28 +0200

    Create AppBar component

--- App.jsx
+++ App.jsx
@@ -3,16 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 // import React from 'react'; // No need it current version of React Native
 
 // import Hello from './src/components/Hello';
+import AppBar from './src/components/AppBar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.appbar}>
-        <View style={styles.appbarInner}>
-          <Text style={styles.appbarTitle}>AnnoyingMemo</Text>
-          <Text style={styles.appbarRight}>Log out</Text>
-        </View>
-      </View>
+
+      <AppBar />
 
       <View style={styles.memoListItem}>
         <View>
@@ -56,28 +53,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F4F8',
-  },
-  appbar: {
-    width: '100%',
-    height: 104,
-    backgroundColor: '#467FD3',
-    justifyContent: 'flex-end',
-  },
-  appbarInner: {
-    alignItems: 'center',
-  },
-  appbarRight: {
-    position: 'absolute',
-    right: 19,
-    bottom: 16,
-    color: 'rgba(255, 255, 255, 0.8)',
-  },
-  appbarTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    color: '#ffffff',
-    fontWeight: 'bold',
   },
   memoListItem: {
     backgroundColor: '#ffffff',
