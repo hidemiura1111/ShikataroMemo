e753be1b8e944ca739109e90287012aac65e87ae
hidems <miura.eleven@gmail.com>
2022-08-22 00:44:29 +0200

    Create MemoListScreen screen

--- App.jsx
+++ App.jsx
@@ -1,25 +1,10 @@
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-// import React from 'react'; // No need it current version of React Native
+import React from 'react'; // No need it current version of React Native
 
-// import Hello from './src/components/Hello';
-import AppBar from './src/components/AppBar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/components/CircleButton';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppBar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-    </View>
+    <MemoListScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F4F8',
-  },
-});
