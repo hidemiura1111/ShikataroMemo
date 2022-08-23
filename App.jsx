a21ee40960df599f559057d3b1b6cf942821f377
hidems <miura.eleven@gmail.com>
2022-08-23 22:12:55 +0200

    Create MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -3,12 +3,14 @@ import React from 'react'; // No need it current version of React Native
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
     // <MemoDetailScreen />
-    <MemoEditScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }
 
