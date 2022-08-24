a77bd5c9e27f5245297591f25d44f96f3a25a3e3
hidems <miura.eleven@gmail.com>
2022-08-24 13:23:16 +0200

    Styling LoginScreen

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -6,17 +6,17 @@ export default function LoginScreen() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <View>
-        <Text>Login</Text>
-        <TextInput value="Email Address" />
-        <TextInput value="Password" />
-      </View>
-      <View>
-        <Text>Submit</Text>
-      </View>
-      <View>
-        <Text>Not Registered?</Text>
-        <Text>Sign yp here!</Text>
+      <View style={styles.inner}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="Password" />
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonLabel}>Submit</Text>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Not Registered?</Text>
+          <Text style={styles.footerLink}>Sign yp here!</Text>
+        </View>
       </View>
     </View>
   );
@@ -25,5 +25,52 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#F0F4F8',
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  buttonContainer: {
+    backgroundColor: '#467FD3',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: '#FFFFFF',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467FD3',
   },
 });
+
