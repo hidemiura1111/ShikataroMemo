58b107b1114ef3b3bae76ca38f93e343dd78702e
hidems <miura.eleven@gmail.com>
2022-08-25 21:51:20 +0200

    Add NavigationContainer

--- App.jsx
+++ App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'; // No need it current version of React Native
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -7,14 +9,15 @@ import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignUpScreen';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <MemoListScreen />
-    // <MemoDetailScreen />
-    // <MemoEditScreen />
-    // <MemoCreateScreen />
-    // <LoginScreen />
-    // <SignupScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={MemoListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
