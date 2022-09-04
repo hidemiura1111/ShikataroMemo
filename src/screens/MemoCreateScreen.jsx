3af55e7845aabed2c7845729f9c6fa4a241cb27d
hidems <miura.eleven@gmail.com>
2022-09-05 01:04:31 +0200

    Add login to iInsert test data in firebase

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,9 +1,26 @@
 import { StyleSheet, TextInput, View } from 'react-native';
+import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 
 export default function MemoCreateScreen(props) {
   const { navigation } = props;
+
+  function handlePress() {
+    const db = firebase.firestore();
+    const ref = db.collection('memos');
+    ref.add({
+      bodyText: 'test',
+    })
+      .then((docRef) => {
+        console.log('Created!', docRef.id);
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log('Error!', error);
+      });
+  }
+
 
   return (
     <View style={styles.container}>
@@ -12,7 +29,7 @@ export default function MemoCreateScreen(props) {
       </View>
       <CircleButton
         name="check"
-        onPress={() => { navigation.goBack(); }}
+        onPress={handlePress}
       />
     </View>
   );
