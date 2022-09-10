1a66f9783384a90ec82432ece634d837a8fd83e1
hidems <miura.eleven@gmail.com>
2022-09-10 23:14:04 +0200

    Add update func in MemoEditScreen

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,22 +1,56 @@
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View, Alert } from 'react-native';
+import { useState } from 'react';
+import { shape, string } from 'prop-types';
+import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 
 export default function MemoEditScreen(props) {
-  const { navigation } = props;
+  const { navigation, route } = props;
+  const { id, bodyText } = route.params;
+  const [body, setBody] = useState(bodyText);
+
+  function handlePress() {
+    const { currentUser } = firebase.auth();
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+      ref.set({
+        bodyText: body,
+        updatedAt: new Date(),
+      }, { merge: true })
+        .then(() => {
+          navigation.goBack();
+        })
+        .catch(() => {
+          Alert.alert('Fail to update memo');
+        });
+    }
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput value="Shopping Lits" multiline style={styles.input} />
+        <TextInput
+          value={body}
+          multiline
+          style={styles.input}
+          onChangeText={(text) => { setBody(text); }}
+        />
       </View>
       <CircleButton
         name="check"
-        onPress={() => { navigation.goBack(); }}
+        onPress={handlePress}
       />
     </View>
   );
 }
+
+MemoEditScreen.propTypes = {
+  route: shape({
+    params: shape({ id: string, bodyText: string }),
+  }).isRequired,
+};
 
 const styles = StyleSheet.create({
   container: {
